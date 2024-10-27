4669e380da0d7babf1b4f8a4ae7833fef315d2c7
bobowg163 <bobowg@163.com>
2024-10-27 22:02:16 +0800

    Compose-desktop Compose for Desktop 可以生成适用于 macOS、Linux 和 Windows 的应用程序。您可以通过我们的模板使用这些平台中的任何一个。

---
+++ src/main/kotlin/HeFeng.kt
@@ -0,0 +1,12 @@
+/**
+ * @作者 bobo
+ * @项目 demo-compose
+ * @日期 2024/10/27 时间 下午8:06
+ * 十月27日星期日
+ */
+
+// 和风天气 Public Id
+const val WEATHER_PUBLIC_ID = "HE2205201110151628"
+
+// 和风天气 Key
+const val WEATHER_KEY = "b766df9be8b148dca6717290db69a739"
---
+++ src/main/kotlin/model/Lang.kt
@@ -0,0 +1,10 @@
+package model
+
+/**
+ * @作者 bobo
+ * @项目 demo-compose
+ * @日期 2024/10/27 时间 下午9:33
+ * 十月27日星期日
+ */
+enum class Lang {
+}
